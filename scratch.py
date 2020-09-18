153ddcf237a452c9892275bf583f67bdbce0cdf8
Rory Murray <rory@tacen.app>
2020-09-18 16:38:14 -0400

    Add sentence exercise

--- scratch.py
+++ scratch.py
@@ -1,6 +1,6 @@
 import re
 
-numberCommaRegex = re.compile(r'(\d{1,3}\D)(\,\d{3})*')
+numberCommaRegex = re.compile(r'(\d{1,3})(\,\d{3})*')
 # numberCommaRegex = re.compile(r'(\d{1,3})(\,)?')
 
 ft = numberCommaRegex.search('hello 1,420,987')
@@ -34,7 +34,7 @@ for item in fArr:
 # 'Haruto watanabe' (where Watanabe is not capitalized)
 
 # watanabeRegex = re.compile(r'([A-Z][A-Za-z]*/s)(Watanabe)')
-watanabeRegex = re.compile(r'([A-Z][a-z]*\s)(Watanabe)')
+watanabeRegex = re.compile(r'([A-Z][a-z]*)+(\sWatanabe)')
 
 w1 = watanabeRegex.search('Haruto Watanabe')
 w2 = watanabeRegex.search('Alice Watanabe')
@@ -50,3 +50,27 @@ print(f'w4 {w4}')
 print(f'w5 {w5}')
 print(f'w6 {w6}')
 
+
+# sentenceRegex = re.compile(r'''(
+#   (Alice|Bob|Carol)
+#   (\seats|\spets|\sthrows)
+#   (\sapples.|\scats.|\sbaseballs.)
+#   )''', re.IGNORECASE)
+
+sentenceRegex = re.compile('(Alice|Bob|Carol)(\seats|\spets|\sthrows)(\sapples.|\scats.|\sbaseballs.)', re.IGNORECASE)
+
+s1 = sentenceRegex.search('Alice eats apples.')
+s2 = sentenceRegex.search('Bob pets cats.')
+s3 = sentenceRegex.search('Carol throws baseballs.')
+s4 = sentenceRegex.search('Alice throws Apples.')
+s5 = sentenceRegex.search('BOB EATS CATS.')
+
+s6 = sentenceRegex.search('RoboCop eats apples.')
+s7 = sentenceRegex.search('ALICE THROWS FOOTBALLS.')
+s8 = sentenceRegex.search('Carol eats 7 cats.')
+
+sArr = [s1, s2, s3, s4, s5, s6, s7, s8]
+
+for item in sArr:
+  if item:
+    print(f'item {item}')
